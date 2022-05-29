bf8c0a00983485b5e171a92815b2f110261c7c41
ambossツ <74627962+ambozz@users.noreply.github.com>
2022-05-29 16:59:52 +0200

    Create bypass.js

---
+++ bypass.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Linkvertise Bypass by amboss.dev
+// @version      1.0
+// @description  Bypasses Linkvertise urls and redirects to the target url.
+// @author       amboss.dev
+// @match        *://*.linkvertise.com/*
+// @match        *://*.linkvertise.net/*
+// @match        *://*.linkvertise.download/*
+// @match        *://*.link-to.net/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://cdn.linkvertise.com/*
+// @exclude      *://link-mutation.linkvertise.com/*
+// @exclude      *://linkvertise.com
+// @exclude      *://linkvertise.com/search*
+// @exclude      *://linkvertise.com/assets*
+// @exclude      *://linkvertise.com/profile*
+// @exclude      *://blog.linkvertise.com
+// @exclude      *://blog.linkvertise.com/*
+// @exclude      https://linkvertise.com/
+// @run-at       document-end
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://linkvertise.com&size=64
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// ==/UserScript==
+
+function reqListener () {
+    console.log(this.responseText);
+    var obj = JSON.parse(this.responseText);
+    console.log(obj.destination);
+    location.href = obj.destination;
+}
+
+setTimeout(function(){
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("POST", "https://api.bypass.vip/");
+    oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    oReq.send("url=" + location.href);
+},50);
